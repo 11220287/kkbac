204a64537b5cc9158ce374b68414caeb0c7c900a
kkbac <11220287@qq.com>
2019-11-17 17:12:46 +0800

    添加项目文件。

---
+++ kkbac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kkbac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
